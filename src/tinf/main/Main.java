d97df3d7afaac0d1c94906caaf6277b324488ae2
Stela <dominik.matic@outlook.com>
2020-01-02 15:32:01 +0100

    dodavanje zadatka u komentar, seems easy tbh

--- src/tinf/main/Main.java
+++ src/tinf/main/Main.java
@@ -1,5 +1,30 @@
 package tinf.main;
 
+/*
+ * ZADATAK: Binarni blok kod K
+ * 
+ * 	Potrebno je implementirati programsko rješenje koje će omogućiti unos proizvoljnog broja
+ * 	kodnih riječi binarnog blok koda K proizvoljne veličine te za uneseni kod odrediti i
+ * 	ispisati sljedeća svojstva:
+ * 
+ * 		1) n i k zadanog koda K
+ * 		2) Udaljenost d(K) koda K([1] str. 132)
+ * 		3) Sposobnost koda da otkrije i ispravi pogrešku, tj. odrediti broj pogrešaka koje kod
+ * 			može otkriti/ispraviti ([1] str. 132)
+ *		4) Je li zadani kod perfektan ([1] str. 136)
+ *		5) Je li zadani kod linearan ([1] str. 142)
+ *
+ */
+
+
+/*
+ * Main izvršava:
+ * 	Unos podataka i provjera njihove ispravnosti
+ * 	pozivanje metoda
+ * 	ispis rješenja
+ * 
+ */
+
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Yoo");
